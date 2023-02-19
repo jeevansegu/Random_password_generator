042a2f268f170b42e9def74c5e439550ed077f63
jeevansegu <jeevansegu03@gmail.com>
2023-02-19 16:25:08 +0530

    changes made in all files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passArray=[]
+let passArray=characters
 let password=""
 let character=""
 let numTog = true
@@ -43,7 +43,9 @@ function symbol() {
 }
 
 
+
 function generate() {
+    passArray = []
     if(symTog === true && numTog === true)
     {
         passArray = passArray.concat(characters,symbols,numbers)
@@ -60,16 +62,28 @@ function generate() {
     {
         passArray = passArray.concat(characters)
     }
-    password1El.textContent = password()
-    password2El.textContent = password()
-}
-
-function password() {
     password = ""
+    character =""
     for(let i=0;i<lenEl.value;i+=1)
     {
         character=passArray[Math.floor(Math.random()*(passArray.length - 1))]
         password += character
     }
-    return password
+    password1El.textContent = password
+    password = ""
+    character =""
+    for(let i=0;i<lenEl.value;i+=1)
+    {
+        character=passArray[Math.floor(Math.random()*(passArray.length - 1))]
+        password += character
+    }
+    password2El.textContent = password
+}
+
+function copy1() {
+    navigator.clipboard.writeText(password1El.textContent)
+}
+
+function copy2() {
+    navigator.clipboard.writeText(password2El.textContent)
 }
